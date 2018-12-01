8e2bc9adabfba604c4f27dc3fe5a83a540844e5e
Anthony DeCenzo <adecenzo@uncc.edu>
2018-12-01 13:45:14 -0500

    Project initialization

---
+++ NinerOps/NinerOps/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NinerOps.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ NinerOps/NinerOps/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NinerOps.Startup))]
+
+namespace NinerOps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
